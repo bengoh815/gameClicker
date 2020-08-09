e3d290a90d8f05cbc6bb360a53749f0fd577cce6
Ben Goh <bengoh@Bens-MacBook-Pro.local>
2020-08-09 23:20:04 +0800

    Target creation
    Made target hit movement. Need to solve whether target was hit within the area

--- gameHeadclicker.py
+++ gameHeadclicker.py
@@ -1,10 +1,29 @@
 import turtle
+from random import randrange
 
 # Screen
 wn=turtle.Screen()
+wn.setup(width=800,height=400)
 wn.title("Head Clicker")
 wn.bgcolor("black")
 wn.tracer(0)
+
+# Target
+target=turtle.Turtle()
+target.color("red")
+target.shape("circle")
+target.penup()
+
+# Target movement
+def hit(x,y):
+    print(x,y)
+    target.goto(randrange(-390, 390), randrange(-190, 190))
+
+# Keyboard binding
+wn.listen()
+wn.onclick(hit)
+
+
 
 while True:
     wn.update()
