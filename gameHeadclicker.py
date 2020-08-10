fd2454560e12114d4753e660374df70f46be2520
Ben Goh <bengoh@Bens-MacBook-Pro.local>
2020-08-10 13:43:50 +0800

    Target hit function
    Solved target hit math related function

--- gameHeadclicker.py
+++ gameHeadclicker.py
@@ -13,11 +13,20 @@ target=turtle.Turtle()
 target.color("red")
 target.shape("circle")
 target.penup()
+targetcoordsx=0
+targetcoordsy=0
 
 # Target movement
 def hit(x,y):
-    print(x,y)
-    target.goto(randrange(-390, 390), randrange(-190, 190))
+    global targetcoordsx, targetcoordsy
+    if withincircle(140, targetcoordsx, targetcoordsy, x, y):
+        targetcoordsx=randrange(-390, 390)
+        targetcoordsy=randrange(-190, 190)
+        target.goto(targetcoordsx, targetcoordsy)
+
+# Check hit
+def withincircle(radius, circlex, circley, pointx, pointy):
+    return (((pointx-circlex)**2+(pointy-circley)**2)<=radius)
 
 # Keyboard binding
 wn.listen()
